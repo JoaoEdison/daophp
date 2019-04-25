739fa520a5c364be282a6a29fc1a0d451f6e3a2d
JoaoEdison <joaoedison342@outlook.com>
2019-04-25 00:36:53 -0300

    métodos de lista

--- index2.php
+++ index2.php
@@ -2,10 +2,23 @@
 
 require_once ("config.php");
 
-$user = new Usuario();
-$user->loadById(9);
+//Carrega um único usuário:
+//
+//$user = new Usuario();
+//$user->loadById(9);
+//echo $user;
 
-echo $user;
+//Carrega uma lista de usuários:
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
+//Carrega uma lista de usuários buscando pelo login:
+//$busca = Usuario::search("Jo");
+//var_dump($busca);
+
+//Carrega um usuário usando o login e a senha:
+$usuario = new Usuario();
+$usuario->login("Joao", "1234590");
+echo $usuario;
 ?>
 
